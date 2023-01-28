a582858990c368b2d4f3a510637ba4d0b3d08667
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-28 17:43:12 +0300

    add photo to home

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from 'react'
+import photo from '../assets/image/photo.jpg'
 
 const Main = () => {
   return (
     <div id='main'>
-      <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://unsplash.com/photos/140jjBjn1Do" alt="fon"/>
+      <img className='w-full h-screen object-cover object-left scale-x-[-1]'
+           src={photo} alt=""/>
       <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
         <div>
           <h1>I'm Gregory Berezin</h1>
