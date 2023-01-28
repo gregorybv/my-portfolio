84639d7bba540a52912e5c9dec4f33c2b98bd30f
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-28 16:16:17 +0300

    sidenav add

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AiOutlineHome, AiOutlineMenu} from "react-icons/all";
 
 const Sidenav = () => {
+  const [nav, setNav] = useState(false)
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <div>
+      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      {
+        nav ? (
+            <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
+              <a href="#main" className='w-[75%] flex justify-center'>
+                <AiOutlineHome size={20}/>
+                <span className='pl-4'>Home</span>
+              </a>
+            </div>
+          )
+          : (
+            <div></div>
+          )
+      }
 
     </div>
   )
