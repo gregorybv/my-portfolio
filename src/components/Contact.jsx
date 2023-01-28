752350cd5d4d52e490f1270854dd00f4d73b0320
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-28 20:02:46 +0300

    add contact block to project

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div id='contact' className='max-w-[1040] m-auto md:pl-20 pl-4 py-16'>
+
+    </div>
+  )
+}
+
+export default Contact
+
+
+
