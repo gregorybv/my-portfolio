ee0076af0250ae8212ca5f164261cac051c1565a
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-28 17:04:36 +0300

    react animation add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Sidenav from "./components/Sidenav.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
 
   return (
     <div>
       <Sidenav/>
+      <Main/>
     </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <div id='main'>
+      <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://unsplash.com/photos/140jjBjn1Do" alt="fon"/>
+      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+        <div>
+          <h1>I'm Gregory Berezin</h1>
+          <h2>I'm a
+
+          </h2>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Main
+
+
+
