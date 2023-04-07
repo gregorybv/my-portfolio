5fb2869ceb6ddb6201b4e28c10346de3d3f9fe3c
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-07 14:23:03 +0300

    new edits

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react"
 
-const ProjectItem = ({img, title, href}) => {
+const ProjectItem = ({ img, title, href, stack }) => {
   return (
     <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
-      <img src={img} alt="/" className='rounded-xl group-hover:opacity-10'/>
+      <img src={img} alt='/' className='h-full rounded-xl group-hover:opacity-10' />
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl font-bold text-white tracking-wide text-center'>
           {title}
         </h3>
-        <p className='pb-4 pt-2 text-white text-center'>React Js</p>
+        <p className='pb-4 pt-2 text-white text-center'>{stack}</p>
         <a href={href} target='_blank'>
-          <p className='text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'>More Info</p>
+          <p className='text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'>
+            More Info
+          </p>
         </a>
       </div>
     </div>
@@ -18,6 +20,3 @@ const ProjectItem = ({img, title, href}) => {
 }
 
 export default ProjectItem
-
-
-
