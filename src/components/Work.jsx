51a8d59d644c9b98ca39fa17c0c0b803e7c5dd95
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-07 12:45:24 +0300

    new edits

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
-import WorkItem from "./WorkItem.jsx";
+import React from "react"
+import WorkItem from "./WorkItem.jsx"
 
 const data = [
   {
-    year: 2022,
-    title: 'Salt&Pepper Agency',
-    duration: '1 Year',
-    details: 'UI design.\n' +
-      'Implementation of new functionality.\n' +
-      'Adaptive and cross-browser layout using HTML, CSS and JavaScript.\n' +
-      'Developing web applications using React.js, project support from the beginning of development to putting them into production.\n' +
-      'Support of completed web projects.\n' +
-      'Interaction with backend developers.'
+    year: "2022 - 2023",
+    title: "Salt&Pepper Agency",
+    duration: "1 Year",
+    details:
+      "• UI design\n" +
+      "• Implementation of new functionality\n" +
+      "• Adaptive and cross-browser layout using HTML, CSS and JavaScript\n" +
+      "• Developing web applications using React.js, project support from the beginning of development to putting them into production\n" +
+      "• Support of completed web projects\n" +
+      "• Interaction with backend developers",
   },
   {
-    year: 2021,
-    title: 'Web Developer Freelance',
-    duration: '1 Year',
-    details: 'Web site layout of various complexity (adaptive, cross-browser, rubber) and other digital projects. Client-side application development using HTML, SCSS, JavaScript. Web application development using React.js. Development of web interface components. Redesign of all sections of the site, design and development of new functionality. Finding the causes of errors on the sites and fixing them'
+    year: "2021 - 2022",
+    title: "Bk company",
+    duration: "1.6 Years",
+    details:
+      "• UI design\n" +
+      "• Implementation of new functionality\n" +
+      "• Adaptive and cross-browser layout using HTML, CSS and JavaScript\n" +
+      "• Developing web applications using React.js, project support from the beginning of development to putting them into production\n" +
+      "• Support of completed web projects\n" +
+      "• Interaction with backend developers",
+  },
+  {
+    year: "2019 - 2021",
+    title: "Alidi",
+    duration: "2 Years",
+    details:
+      "• Creation and administration of local network\n" +
+      "• Configuration and maintenance of trade equipment\n" +
+      "• PC maintenance (routine maintenance, diagnostics, repair, new pc assembly, software installation and configuration)",
   },
 ]
 const Work = () => {
   return (
-    <div id='work' className='max-w-[1040] m-auto md:pl-20 pt-12 py-16'>
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 pt-12 py-16'>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
       {data.map((item, idx) => (
         <WorkItem
@@ -30,13 +46,11 @@ const Work = () => {
           year={item.year}
           title={item.title}
           duration={item.duration}
-          details={item.details}/>
+          details={item.details}
+        />
       ))}
     </div>
   )
 }
 
 export default Work
-
-
-
