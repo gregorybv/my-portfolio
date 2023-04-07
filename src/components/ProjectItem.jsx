287d142b92115e1de9e20f9b0d90e2232f25c94a
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-07 12:45:18 +0300

    new edits

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -10,7 +10,7 @@ const ProjectItem = ({img, title, href}) => {
         </h3>
         <p className='pb-4 pt-2 text-white text-center'>React Js</p>
         <a href={href} target='_blank'>
-          <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'>More Info</p>
+          <p className='text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg'>More Info</p>
         </a>
       </div>
     </div>
