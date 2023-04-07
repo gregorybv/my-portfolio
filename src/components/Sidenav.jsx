8337481fbf6e94d16798cbdba492791eef9d5302
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-07 19:12:25 +0300

    add aos

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,13 @@
-import React, {useState} from 'react';
-import {AiOutlineHome, AiOutlineMail, AiOutlineMenu, AiOutlineProject, BsPerson, GrProjects} from "react-icons/all";
+import React, { useState } from "react"
+import {
+  AiOutlineHome,
+  AiOutlineMail,
+  AiOutlineMenu,
+  AiOutlineProject,
+  BsPerson,
+  GrProjects,
+  AiOutlineAppstore,
+} from "react-icons/all"
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false)
@@ -9,74 +17,118 @@ const Sidenav = () => {
 
   return (
     <div>
-      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <AiOutlineMenu
+        onClick={handleNav}
+        className='absolute top-4 right-4 z-[99] md:hidden'
+      />
       {nav ? (
         <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-          <a onClick={handleNav}
-             href="#main"
-             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
-            <AiOutlineHome size={20}/>
+          <a
+            onClick={handleNav}
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineHome size={20} />
             <span className='pl-4'>Home</span>
           </a>
-          <a onClick={handleNav}
-             href="#work"
-             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
-            <GrProjects size={20}/>
+          <a
+            onClick={handleNav}
+            href='#work'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineAppstore size={20} />
             <span className='pl-4'>Work</span>
           </a>
-          <a onClick={handleNav}
-             href="#projects"
-             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
-            <AiOutlineProject size={20}/>
+          <a
+            onClick={handleNav}
+            href='#projects'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineProject size={20} />
             <span className='pl-4'>Projects</span>
           </a>
-          <a onClick={handleNav}
-             href="#main"
-             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
-            <BsPerson size={20}/>
+          <a
+            onClick={handleNav}
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <BsPerson size={20} />
             <span className='pl-4'>Resume</span>
           </a>
-          <a onClick={handleNav}
-             href="#contact"
-             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'>
-            <AiOutlineMail size={20}/>
+          <a
+            onClick={handleNav}
+            href='#contact'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineMail size={20} />
             <span className='pl-4'>Contact</span>
           </a>
         </div>
       ) : (
-        ''
+        ""
       )}
 
       <div className='md:block hidden fixed top-[25%] z-10'>
         <div className='flex flex-col'>
-          <a href="#main"
-             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-            <AiOutlineHome size={20}/>
+          <a
+            href='#main'
+            className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
+            data-aos='flip-left'
+            data-aos-duration='1300'
+          >
+            <AiOutlineHome
+              size={20}
+              className='hover:scale-150 ease-in duration-300'
+            />
           </a>
-          <a href="#work"
-             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-            <GrProjects size={20}/>
+          <a
+            href='#work'
+            className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
+            data-aos='flip-left'
+            data-aos-duration='1300'
+          >
+            <AiOutlineAppstore
+              size={20}
+              className='hover:scale-150 ease-in duration-300'
+            />
           </a>
-          <a href="#projects"
-             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-            <AiOutlineProject size={20}/>
+          <a
+            href='#projects'
+            className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
+            data-aos='flip-left'
+            data-aos-duration='1300'
+          >
+            <AiOutlineProject
+              size={20}
+              className='hover:scale-150 ease-in duration-300'
+            />
           </a>
-          <a href="#main"
-             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-            <BsPerson size={20}/>
+          <a
+            href='#main'
+            className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
+            data-aos='flip-left'
+            data-aos-duration='1300'
+          >
+            <BsPerson
+              size={20}
+              className='hover:scale-150 ease-in duration-300'
+            />
           </a>
-          <a href="#contact"
-             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
-            <AiOutlineMail size={20}/>
+          <a
+            href='#contact'
+            className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
+            data-aos='flip-left'
+            data-aos-duration='1300'
+          >
+            <AiOutlineMail
+              size={20}
+              className='hover:scale-150 ease-in duration-300'
+            />
           </a>
         </div>
       </div>
-
     </div>
   )
 }
 
 export default Sidenav
-
-
-
