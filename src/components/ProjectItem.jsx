66a1825fcb211fbf1f50c786cdec3546ea2f403a
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-08 10:40:10 +0300

    new shadow

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const ProjectItem = ({ img, title, href, stack }) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e]'>
+    <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-[#4c85e6] rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] hover:scale-110'>
       <img src={img} alt='/' className='h-full rounded-xl group-hover:opacity-10' />
       <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <h3 className='text-2xl font-bold text-white tracking-wide text-center'>
