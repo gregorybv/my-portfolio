6eed6a959da09a8368dd04423bb2ff2a6456c17f
Gregory <gregory_berezin@outlook.com>
2023-05-12 12:45:53 +0300

    add comments

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+// import AOS library
 import AOS from "aos"
 import "aos/dist/aos.css"
 
+// add AOS init
 AOS.init()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
