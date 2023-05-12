4d5e2103088614c60be2b83d7bd8ca09e03e2e27
Gregory <gregory_berezin@outlook.com>
2023-05-12 20:26:25 +0300

    new edits

---
+++ src/components/UI/SidenavItem.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { navbar } from "../../data"
+
+const SidenavItem = () => {
+  return (
+    <div className='md:block hidden fixed top-[25%] z-10 '>
+      <div className='flex flex-col'>
+        {navbar.map((item, id) => {
+          const { href, Icon } = item
+          return (
+            <a
+              key={id}
+              href={href}
+              className='group rounded-full shadow-lg bg-gray-100 shadow-[#4c85e6] m-2 p-4 cursor-pointer'
+              data-aos='flip-left'
+              data-aos-duration='1300'
+            >
+              <Icon
+                size={20}
+                className='group-hover:scale-150 ease-in duration-300 group-hover:text-[#08fdd8]'
+              />
+            </a>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default SidenavItem
