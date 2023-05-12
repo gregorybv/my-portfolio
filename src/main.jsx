2edc1923019f5b22c1dad6abe2201eaededfd80b
Gregory <gregory_berezin@outlook.com>
2023-05-12 20:26:15 +0300

    new edits

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
-import "./index.css"
+import "./style/index.css"
 // import AOS library
 import AOS from "aos"
 import "aos/dist/aos.css"
