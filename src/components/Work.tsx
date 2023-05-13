411ff1cafd932d00e4673ebc6b2a7a8690c840f8
Gregory <gregory_berezin@outlook.com>
2023-05-13 12:21:43 +0300

    new edits

---
+++ src/components/Work.tsx
@@ -0,0 +1,24 @@
+// Work component
+import WorkItem from "./WorkItem"
+import { work } from "../data"
+
+const Work: React.FC = () => {
+  return (
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 pt-12 py-16'>
+      <h2 className='h2'>
+        Work
+      </h2>
+      {work.map((item, idx) => (
+        <WorkItem
+          key={idx}
+          year={item.year}
+          title={item.title}
+          duration={item.duration}
+          details={item.details}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default Work
