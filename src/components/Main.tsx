43a903539f507b354f1db975daa10917dffcd80e
Gregory <gregory_berezin@outlook.com>
2023-05-13 12:21:27 +0300

    new edits

---
+++ src/components/Main.tsx
@@ -0,0 +1,21 @@
+import Cursor from "./UI/Cursor"
+import Sidenav from "./Sidenav"
+import Hero from "./Hero"
+import Work from "./Work"
+import Projects from "./Projects"
+import Contact from "./Contact"
+
+const Main: React.FC = () => {
+  return (
+    <div>
+      <Cursor />
+      <Sidenav />
+      <Hero />
+      <Work />
+      <Projects />
+      <Contact />
+    </div>
+  )
+}
+
+export default Main
