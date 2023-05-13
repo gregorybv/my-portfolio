a3b1955d67c51b3498fd87a4c70e2c7a560285af
Gregory <gregory_berezin@outlook.com>
2023-05-13 12:21:21 +0300

    new edits

---
+++ src/components/Contact.tsx
@@ -0,0 +1,14 @@
+// Contact component
+import ContactForm from "./UI/ContactForm"
+
+const Contact: React.FC = () => {
+  return (
+    <div id='contact' className='max-w-[900px] m-auto md:pl-20 px-4 py-16'>
+      <h2 className='h2'>Contact me</h2>
+      {/* add form */}
+      <ContactForm />
+    </div>
+  )
+}
+
+export default Contact
