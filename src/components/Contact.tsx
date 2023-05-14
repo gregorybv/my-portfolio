08889e61b550e96a3dc8b29653c124aebc079d94
Gregory <gregory_berezin@outlook.com>
2023-05-14 10:07:47 +0300

    add comments

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ const Contact: React.FC = () => {
   return (
     <div id='contact' className='max-w-[900px] m-auto md:pl-20 px-4 py-16'>
       <h2 className='h2'>Contact me</h2>
-      {/* add form */}
+      {/* plugging in the component ContactForm */}
       <ContactForm />
     </div>
   )
