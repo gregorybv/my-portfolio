8ad1c72644a61d04746728fcfcc84d2c4ca48455
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:39 +0300

    new edits

--- src/components/photo.jpg.d.ts
+++ src/components/photo.jpg.d.ts
@@ -1,5 +1,5 @@
 // This code removes the error: Cannot find module jpg or its corresponding type declarations.ts(2307)
-declare module "*.jpg" {
+declare module '*.jpg' {
   const value: any;
   export default value;
 }
