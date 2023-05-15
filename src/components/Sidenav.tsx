d52fa20771ad76cdc3c72ed5df060a9e8d409eef
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:50 +0300

    new edits

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,28 +1,28 @@
 // Sidenav component
-import { useState } from "react"
-import { AiOutlineMenu } from "react-icons/all"
-import SidenavMobile from "./UI/SidenavMobile"
-import SidenavItem from "./UI/SidenavItem"
+import { useState } from 'react';
+import { AiOutlineMenu } from 'react-icons/all';
+import SidenavMobile from './UI/SidenavMobile';
+import SidenavItem from './UI/SidenavItem';
 
 const Sidenav: React.FC = () => {
-  const [nav, setNav] = useState(false)
+  const [nav, setNav] = useState(false);
   const handleNav = () => {
-    setNav(!nav)
-  }
+    setNav(!nav);
+  };
 
   return (
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className='fixed top-5 right-5 z-[99] md:hidden'
+        className="fixed top-5 right-5 z-[99] md:hidden"
         size={30}
       />
       {/* plugging in the component SidenavMobile */}
-      {nav ? <SidenavMobile handleNav={handleNav} /> : ""}
+      {nav ? <SidenavMobile handleNav={handleNav} /> : ''}
       {/* plugging in the component SidenavItem */}
       <SidenavItem />
     </div>
-  )
-}
+  );
+};
 
-export default Sidenav
+export default Sidenav;
