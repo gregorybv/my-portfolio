0fe578737f92a600ba448bffd6c7d1760be2ee5f
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:30 +0300

    new edits

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,14 @@
 // Contact component
-import ContactForm from "./UI/ContactForm"
+import ContactForm from './UI/ContactForm';
 
 const Contact: React.FC = () => {
   return (
-    <div id='contact' className='max-w-[900px] m-auto md:pl-20 px-4 py-16'>
-      <h2 className='h2'>Contact me</h2>
+    <div id="contact" className="max-w-[900px] m-auto md:pl-20 px-4 py-16">
+      <h2 className="h2">Contact me</h2>
       {/* plugging in the component ContactForm */}
       <ContactForm />
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
