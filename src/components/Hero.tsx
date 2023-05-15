72b69326b99c876dc4d9ee6d020855e868f3a5de
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:33 +0300

    new edits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,19 @@
 // Main component
-import photo from "../assets/image/photo.jpg"
-import HeroLink from "./UI/HeroLink"
-import HeroAnimation from "./UI/HeroAnimation"
-import HeroTitle from "./UI/HeroTitle"
+import photo from '../assets/image/photo.jpg';
+import HeroLink from './UI/HeroLink';
+import HeroAnimation from './UI/HeroAnimation';
+import HeroTitle from './UI/HeroTitle';
 
 const Hero: React.FC = () => {
-
   return (
-    <div id='main'>
+    <div id="main">
       <img
-        className='w-full h-screen object-cover object-left scale-x-[-1]'
+        className="w-full h-screen object-cover object-left scale-x-[-1]"
         src={photo}
-        alt='/'
+        alt="/"
       />
-      <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
-        <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
           {/* plugging in the component HeroTitle */}
           <HeroTitle />
           {/* plugging in the component HeroAnimation */}
@@ -24,7 +23,7 @@ const Hero: React.FC = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
