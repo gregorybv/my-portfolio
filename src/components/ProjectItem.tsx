d6a840c488b5eec2d521a67dfdce1106802cf783
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:42 +0300

    new edits

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -7,28 +7,33 @@ interface ProjectItemProps {
   stack: string;
 }
 
-const ProjectItem: React.FC<ProjectItemProps> = ({ img, title, href, stack }) => {
+const ProjectItem: React.FC<ProjectItemProps> = ({
+  img,
+  title,
+  href,
+  stack,
+}) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-lg shadow-[#4c85e6] rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] hover:scale-110 ease-in duration-300'>
+    <div className="relative flex items-center justify-center h-auto w-full shadow-lg shadow-[#4c85e6] rounded-xl group hover:bg-gradient-to-r from-gray-200 to-[#001b5e] hover:scale-110 ease-in duration-300">
       <img
         src={img}
-        alt='projectitemImage'
-        className='h-full rounded-xl group-hover:opacity-10'
-        loading='lazy'
+        alt="projectitemImage"
+        className="h-full rounded-xl group-hover:opacity-10"
+        loading="lazy"
       />
-      <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='text-2xl font-bold text-white tracking-wide text-center'>
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl font-bold text-white tracking-wide text-center">
           {title}
         </h3>
-        <p className='pb-4 pt-2 text-white text-center'>{stack}</p>
-        <a href={href} target='_blank'>
-          <p className='text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg hover:bg-[#001b5e] hover:text-white'>
+        <p className="pb-4 pt-2 text-white text-center">{stack}</p>
+        <a href={href} target="_blank">
+          <p className="text-center p-2 rounded-lg bg-white text-gray-700 font-bold cursor-pointer text-lg hover:bg-[#001b5e] hover:text-white">
             More Info
           </p>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectItem
+export default ProjectItem;
