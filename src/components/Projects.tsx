1712b0a4e63d729e9e3253408ef97b74d49dc401
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:46 +0300

    new edits

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,32 +1,36 @@
 // Projects component
-import ProjectItem from "./ProjectItem"
-import { project } from "../data"
-import { SmilingFace } from "fluent-emoji"
+import ProjectItem from './ProjectItem';
+import { project } from '../data';
+import { SmilingFace } from 'fluent-emoji';
 
 const Projects: React.FC = () => {
   return (
     <div
-      id='projects'
-      className='max-w-[1240px] m-auto md:pl-20 pl-4 pr-4 py-10'
+      id="projects"
+      className="max-w-[1240px] m-auto md:pl-20 pl-4 pr-4 py-10"
     >
-      <h2 className='h2'>
-        Projects
-      </h2>
-      <p className='flex items-center justify-center gap-3 text-center py-8 text-2xl'>
+      <h2 className="h2">Projects</h2>
+      <p className="flex items-center justify-center gap-3 text-center py-8 text-2xl">
         Examples of my recent projects implemented using various technologies
-        <SmilingFace className='w-[40px]' />
+        <SmilingFace className="w-[40px]" />
       </p>
-      <div className='grid sm:grid-cols-3 gap-6 mt-5'>
+      <div className="grid sm:grid-cols-3 gap-6 mt-5">
         {project.map((item, id) => {
-          const { img, title, stack, href } = item
+          const { img, title, stack, href } = item;
           return (
             // plugging in the component ProjectItem
-            <ProjectItem key={id} img={img} title={title} stack={stack} href={href} />
-          )
+            <ProjectItem
+              key={id}
+              img={img}
+              title={title}
+              stack={stack}
+              href={href}
+            />
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
