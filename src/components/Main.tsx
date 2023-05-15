da4b9def0308e056890d92d9c909eae4cb73c4f4
Gregory <gregory_berezin@outlook.com>
2023-05-15 21:13:35 +0300

    new edits

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,9 @@
-import Cursor from "./UI/Cursor"
-import Sidenav from "./Sidenav"
-import Hero from "./Hero"
-import Work from "./Work"
-import Projects from "./Projects"
-import Contact from "./Contact"
+import Cursor from './UI/Cursor';
+import Sidenav from './Sidenav';
+import Hero from './Hero';
+import Work from './Work';
+import Projects from './Projects';
+import Contact from './Contact';
 
 const Main: React.FC = () => {
   return (
@@ -15,7 +15,7 @@ const Main: React.FC = () => {
       <Projects />
       <Contact />
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
