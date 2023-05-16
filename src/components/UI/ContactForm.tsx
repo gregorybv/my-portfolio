0b98a754c6ce63502d55902ce2be060b7daf1be9
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:22:36 +0300

    new edits

--- src/components/UI/ContactForm.tsx
+++ src/components/UI/ContactForm.tsx
@@ -1,61 +1,61 @@
-import FormButton from "./FormButton"
+import FormButton from './FormButton';
 
 const ContactForm: React.FC = () => {
   return (
     <form
-      action='https://getform.io/f/da699e76-08d8-4b86-8287-fb489777d6b9'
-      method='POST'
-      encType='multipart/form-data'
+      action="https://getform.io/f/da699e76-08d8-4b86-8287-fb489777d6b9"
+      method="POST"
+      encType="multipart/form-data"
     >
-      <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-        <div className='flex flex-col'>
-          <label className='uppercase text-sm py-2'>Name</label>
+      <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+        <div className="flex flex-col">
+          <label className="uppercase text-sm py-2">Name</label>
           <input
-            className='border-2 rounded-lg p-3 flex border-gray-300'
-            type='text'
-            name='name'
+            className="border-2 rounded-lg p-3 flex border-gray-300"
+            type="text"
+            name="name"
           />
         </div>
 
-        <div className='flex flex-col'>
-          <label className='uppercase text-sm py-2'>Phone Number</label>
+        <div className="flex flex-col">
+          <label className="uppercase text-sm py-2">Phone Number</label>
           <input
-            className='border-2 rounded-lg p-3 flex border-gray-300 '
-            type='text'
-            name='phone'
+            className="border-2 rounded-lg p-3 flex border-gray-300 "
+            type="text"
+            name="phone"
           />
         </div>
       </div>
 
-      <div className='flex flex-col py-2'>
-        <label className='uppercase text-sm py-2'>Email</label>
+      <div className="flex flex-col py-2">
+        <label className="uppercase text-sm py-2">Email</label>
         <input
-          className='border-2 rounded-lg p-3 flex border-gray-300'
-          type='email'
-          name='name'
+          className="border-2 rounded-lg p-3 flex border-gray-300"
+          type="email"
+          name="name"
         />
       </div>
 
-      <div className='flex flex-col py-2'>
-        <label className='uppercase text-sm py-2'>Subject</label>
+      <div className="flex flex-col py-2">
+        <label className="uppercase text-sm py-2">Subject</label>
         <input
-          className='border-2 rounded-lg p-3 flex border-gray-300 '
-          type='text'
-          name='subject'
+          className="border-2 rounded-lg p-3 flex border-gray-300 "
+          type="text"
+          name="subject"
         />
       </div>
 
-      <div className='flex flex-col py-2'>
-        <label className='uppercase text-sm py-2'>Message</label>
+      <div className="flex flex-col py-2">
+        <label className="uppercase text-sm py-2">Message</label>
         <textarea
-          className='border-2 rounded-lg p-3 border-gray-300'
+          className="border-2 rounded-lg p-3 border-gray-300"
           rows={5}
-          name='message'
+          name="message"
         ></textarea>
       </div>
       <FormButton />
     </form>
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
