00247449e9207b2e1964f07fa67ee8c99277c7f8
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:22:32 +0300

    new edits

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-import "./style/index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style/index.css';
 // import AOS library
-import AOS from 'aos'
-import "aos/dist/aos.css"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // add AOS init
-AOS.init()
+AOS.init();
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
