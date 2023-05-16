b359dae6527fc803a1971a53509cb9e95328e32e
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:22:42 +0300

    new edits

--- src/components/UI/FormButton.tsx
+++ src/components/UI/FormButton.tsx
@@ -1,12 +1,11 @@
-
 const FormButton: React.FC = () => {
   return (
-    <button className='bg-[#001b5e] mt-4 w-full p-4 rounded-lg shadow-lg shadow-[#4c85e6] group'>
-      <p className='text-gray-100 text-xl group-hover:scale-110 ease-in duration-200'>
+    <button className="bg-[#001b5e] mt-4 w-full p-4 rounded-lg shadow-lg shadow-[#4c85e6] group">
+      <p className="text-gray-100 text-xl group-hover:scale-110 ease-in duration-200">
         Send Message
       </p>
     </button>
-  )
-}
+  );
+};
 
-export default FormButton
+export default FormButton;
