97a5d406676fcb6565e4dfdbf121ad58957a36cb
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:22:38 +0300

    new edits

--- src/components/UI/Cursor.tsx
+++ src/components/UI/Cursor.tsx
@@ -1,7 +1,7 @@
 // Cursor component
 
 // add animatedcursor
-import AnimatedCursor from "react-animated-cursor"
+import AnimatedCursor from 'react-animated-cursor';
 
 const Cursor: React.FC = () => {
   return (
@@ -10,26 +10,26 @@ const Cursor: React.FC = () => {
       <AnimatedCursor
         innerSize={12}
         outerSize={10}
-        color='0, 0, 0'
+        color="0, 0, 0"
         outerAlpha={0.2}
         innerScale={0.8}
         outerScale={5}
         clickables={[
-          "a",
+          'a',
           'input[type="text"]',
           'input[type="email"]',
           'input[type="number"]',
           'input[type="submit"]',
           'input[type="image"]',
-          "label[for]",
-          "select",
-          "textarea",
-          "button",
-          ".link",
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link',
         ]}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Cursor
+export default Cursor;
