bdec6747f0dba1f86709aef3c48968c2d7bda488
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:23:00 +0300

    new edits

--- src/components/UI/SidenavMobile.tsx
+++ src/components/UI/SidenavMobile.tsx
@@ -1,4 +1,4 @@
-import { navbarmin } from "../../data"
+import { navbarmin } from '../../data';
 
 interface SidenavMobileProps {
   handleNav: () => void;
@@ -6,23 +6,23 @@ interface SidenavMobileProps {
 
 const SidenavMobile: React.FC<SidenavMobileProps> = ({ handleNav }) => {
   return (
-    <div className='fixed w-full h-screen bg-[#e3f6e0] flex flex-col justify-center items-center z-20'>
+    <div className="fixed w-full h-screen bg-[#e3f6e0] flex flex-col justify-center items-center z-20">
       {navbarmin.map((item, id) => {
-        const { href, Iconmin, name } = item
+        const { href, Iconmin, name } = item;
         return (
-          <a key={id} onClick={handleNav} href={href} className='mobile group'>
+          <a key={id} onClick={handleNav} href={href} className="mobile group">
             <Iconmin
               size={20}
-              className='group-hover:text-[#08fdd8] group-hover:scale-105 ease-in duration-200'
+              className="group-hover:text-[#08fdd8] group-hover:scale-105 ease-in duration-200"
             />
-            <span className='pl-4 group-hover:text-[#08fdd8] group-hover:scale-105 ease-in duration-200'>
+            <span className="pl-4 group-hover:text-[#08fdd8] group-hover:scale-105 ease-in duration-200">
               {name}
             </span>
           </a>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default SidenavMobile
+export default SidenavMobile;
