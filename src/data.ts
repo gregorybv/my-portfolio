b9de1d67e545accffab8d7b1ea0070166d2ec52b
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:22:29 +0300

    new edits

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,12 @@
-import img1 from "./assets/image/1.png"
-import img2 from "./assets/image/2.png"
-import img3 from "./assets/image/3.png"
-import img4 from "./assets/image/4.png"
-import img5 from "./assets/image/5.png"
-import img6 from "./assets/image/6.png"
-import img7 from "./assets/image/7.png"
-import img8 from "./assets/image/8.png"
-import img9 from "./assets/image/9.png"
+import img1 from './assets/image/1.png';
+import img2 from './assets/image/2.png';
+import img3 from './assets/image/3.png';
+import img4 from './assets/image/4.png';
+import img5 from './assets/image/5.png';
+import img6 from './assets/image/6.png';
+import img7 from './assets/image/7.png';
+import img8 from './assets/image/8.png';
+import img9 from './assets/image/9.png';
 
 import {
   AiOutlineHome,
@@ -14,8 +14,7 @@ import {
   AiOutlineProject,
   AiOutlineAppstore,
   AiOutlineArrowUp,
-} from "react-icons/all"
-
+} from 'react-icons/all';
 
 interface Project {
   img: string;
@@ -46,142 +45,142 @@ interface Navbarmin {
 export const project: Project[] = [
   {
     img: img1,
-    title: "Sports site",
-    stack: "react css",
-    href: "https://gregorybv.github.io/gym-website-react/",
+    title: 'Sports site',
+    stack: 'react css',
+    href: 'https://gregorybv.github.io/gym-website-react/',
   },
   {
     img: img2,
-    title: "Your tour",
-    stack: "react hooks css aos",
-    href: "https://gregorybv.github.io/tour-app-react/",
+    title: 'Your tour',
+    stack: 'react hooks css aos',
+    href: 'https://gregorybv.github.io/tour-app-react/',
   },
   {
     img: img3,
-    title: "Weekaway site",
-    stack: "react tailwindcss",
-    href: "https://gregorybv.github.io/weekaway-website/",
+    title: 'Weekaway site',
+    stack: 'react tailwindcss',
+    href: 'https://gregorybv.github.io/weekaway-website/',
   },
   {
     img: img4,
-    title: "Music app",
-    stack: "react rrd tailwindcss",
-    href: "https://gregorybv.github.io/music-app/",
+    title: 'Music app',
+    stack: 'react rrd tailwindcss',
+    href: 'https://gregorybv.github.io/music-app/',
   },
   {
     img: img5,
-    title: "Grow with data",
-    stack: "react tailwindcss",
-    href: "https://gregorybv.github.io/grow-with-data/",
+    title: 'Grow with data',
+    stack: 'react tailwindcss',
+    href: 'https://gregorybv.github.io/grow-with-data/',
   },
   {
     img: img6,
-    title: "Drive site",
-    stack: "react css hooks",
-    href: "https://gregorybv.github.io/drive-website/",
+    title: 'Drive site',
+    stack: 'react css hooks',
+    href: 'https://gregorybv.github.io/drive-website/',
   },
   {
     img: img7,
-    title: "Your travel",
-    stack: "react tailwind hooks",
-    href: "https://gregorybv.github.io/travel-app/",
+    title: 'Your travel',
+    stack: 'react tailwind hooks',
+    href: 'https://gregorybv.github.io/travel-app/',
   },
   {
     img: img8,
-    title: "Trading Protocol",
-    stack: "react tailwind hooks",
-    href: "https://gregorybv.github.io/trading-protocol/",
+    title: 'Trading Protocol',
+    stack: 'react tailwind hooks',
+    href: 'https://gregorybv.github.io/trading-protocol/',
   },
   {
     img: img9,
-    title: "Kraftex site",
-    stack: "react css hooks",
-    href: "https://gregorybv.github.io/kraftex-app/",
+    title: 'Kraftex site',
+    stack: 'react css hooks',
+    href: 'https://gregorybv.github.io/kraftex-app/',
   },
-]
+];
 
 // work data
 export const work: Work[] = [
   {
-    year: "2022 - 2023",
-    title: "Salt&Pepper Agency",
-    duration: "1 Year",
+    year: '2022 - 2023',
+    title: 'Salt&Pepper Agency',
+    duration: '1 Year',
     details:
-      "• UI design\n" +
-      "• Implementation of new functionality\n" +
-      "• Adaptive and cross-browser layout using HTML, CSS and JavaScript\n" +
-      "• Developing web applications using React.js, project support from the beginning of development to putting them into production\n" +
-      "• Support of completed web projects\n" +
-      "• Interaction with backend developers",
+      '• UI design\n' +
+      '• Implementation of new functionality\n' +
+      '• Adaptive and cross-browser layout using HTML, CSS and JavaScript\n' +
+      '• Developing web applications using React.js, project support from the beginning of development to putting them into production\n' +
+      '• Support of completed web projects\n' +
+      '• Interaction with backend developers',
   },
   {
-    year: "2021 - 2022",
-    title: "Bk company",
-    duration: "1.6 Years",
+    year: '2021 - 2022',
+    title: 'Bk company',
+    duration: '1.6 Years',
     details:
-      "• UI design\n" +
-      "• Implementation of new functionality\n" +
-      "• Adaptive and cross-browser layout using HTML, CSS and JavaScript\n" +
-      "• Developing web applications using React.js, project support from the beginning of development to putting them into production\n" +
-      "• Support of completed web projects\n" +
-      "• Interaction with backend developers",
+      '• UI design\n' +
+      '• Implementation of new functionality\n' +
+      '• Adaptive and cross-browser layout using HTML, CSS and JavaScript\n' +
+      '• Developing web applications using React.js, project support from the beginning of development to putting them into production\n' +
+      '• Support of completed web projects\n' +
+      '• Interaction with backend developers',
   },
   {
-    year: "2019 - 2021",
-    title: "Alidi",
-    duration: "2 Years",
+    year: '2019 - 2021',
+    title: 'Alidi',
+    duration: '2 Years',
     details:
-      "• Creation and administration of local network\n" +
-      "• Configuration and maintenance of trade equipment\n" +
-      "• PC maintenance (routine maintenance, diagnostics, repair, new pc assembly, software installation and configuration)\n" +
-      "• User support",
+      '• Creation and administration of local network\n' +
+      '• Configuration and maintenance of trade equipment\n' +
+      '• PC maintenance (routine maintenance, diagnostics, repair, new pc assembly, software installation and configuration)\n' +
+      '• User support',
   },
-]
+];
 
 // nawbar data
 export const navbar: Navbar[] = [
   {
-    href: "#main",
+    href: '#main',
     Icon: AiOutlineHome,
   },
   {
-    href: "#work",
+    href: '#work',
     Icon: AiOutlineAppstore,
   },
   {
-    href: "#projects",
+    href: '#projects',
     Icon: AiOutlineProject,
   },
   {
-    href: "#contact",
+    href: '#contact',
     Icon: AiOutlineMail,
   },
   {
-    href: "#main",
+    href: '#main',
     Icon: AiOutlineArrowUp,
   },
-]
+];
 
 // navbarmin data
 export const navbarmin: Navbarmin[] = [
   {
-    href: "#main",
+    href: '#main',
     Iconmin: AiOutlineHome,
-    name: "Home",
+    name: 'Home',
   },
   {
-    href: "#work",
+    href: '#work',
     Iconmin: AiOutlineAppstore,
-    name: "Work",
+    name: 'Work',
   },
   {
-    href: "#projects",
+    href: '#projects',
     Iconmin: AiOutlineProject,
-    name: "Projects",
+    name: 'Projects',
   },
   {
-    href: "#contact",
+    href: '#contact',
     Iconmin: AiOutlineMail,
-    name: "Contact",
+    name: 'Contact',
   },
-]
+];
