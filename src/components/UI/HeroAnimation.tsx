2c3a68826e9057800e1b0a06af8f8e86c50f9f3b
Gregory <gregory_berezin@outlook.com>
2023-05-16 19:22:45 +0300

    new edits

--- src/components/UI/HeroAnimation.tsx
+++ src/components/UI/HeroAnimation.tsx
@@ -1,34 +1,34 @@
-import { WinkingFace } from 'fluent-emoji'
-import React from 'react'
-import { TypeAnimation } from 'react-type-animation'
+import { WinkingFace } from 'fluent-emoji';
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroAnimation: React.FC = () => {
   return (
     <h2
-      className='flex items-center sm:text-4xl text-2xl pt-4 text-gray-800'
-      data-aos='fade-up'
-      data-aos-duration='1300'
+      className="flex items-center sm:text-4xl text-2xl pt-4 text-gray-800"
+      data-aos="fade-up"
+      data-aos-duration="1300"
     >
       <TypeAnimation
         sequence={[
-          "Developer",
+          'Developer',
           2000,
-          "Coder",
+          'Coder',
           2000,
-          "Technical enthusiast",
+          'Technical enthusiast',
           2000,
-          "And just a good man",
+          'And just a good man',
           2000,
         ]}
-        wrapper='div'
+        wrapper="div"
         cursor={false}
         repeat={Infinity}
-        style={{ fontSize: "1em", paddingLeft: "5px" }}
+        style={{ fontSize: '1em', paddingLeft: '5px' }}
       />
       {/* emoji */}
-      <WinkingFace className='w-[55px] pl-3' />
+      <WinkingFace className="w-[55px] pl-3" />
     </h2>
-  )
-}
+  );
+};
 
-export default HeroAnimation
+export default HeroAnimation;
