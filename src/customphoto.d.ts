c17476268c2d948fa28b9e41e49a7915afba1c98
Gregory <gregory_berezin@outlook.com>
2023-05-19 21:24:14 +0300

    new edits

--- src/customphoto.d.ts
+++ src/customphoto.d.ts
@@ -1,5 +1,4 @@
 // This code removes the error: Cannot find module './assets/image/9.png' or its corresponding type declarations.ts(2307)
-
 declare module '*.png' {
   const value: any;
   export default value;
