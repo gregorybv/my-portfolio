5ca148645b7f63a04f54acd0c516e35d280c6f2b
Gregory <gregory_berezin@outlook.com>
2023-05-19 21:24:21 +0300

    new edits

--- src/components/UI/TextSpan.tsx
+++ src/components/UI/TextSpan.tsx
@@ -1,43 +1,43 @@
 // TextSpan component
 // add motion
-import { motion, useAnimationControls } from "framer-motion"
-import { useState, ReactNode } from "react"
+import { motion, useAnimationControls } from 'framer-motion';
+import { useState, ReactNode } from 'react';
 
 interface TextSpanProps {
   children: ReactNode;
-  className: string
+  className: string;
 }
 
 const TextSpan: React.FC<TextSpanProps> = ({ children }) => {
-  const controls = useAnimationControls()
-  const [isPlaying, setIsPlaying] = useState(false)
+  const controls = useAnimationControls();
+  const [isPlaying, setIsPlaying] = useState(false);
 
   const rubberBand = () => {
     controls.start({
       transform: [
-        "scale3d(1, 1, 1)",
-        "scale3d(1.4, .55, 1)",
-        "scale3d(.75, 1.25, 1)",
-        "scale3d(1.25, .85, 1)",
-        "scale3d(.9, 1.05, 1)",
-        "scale3d(1, 1, 1)",
+        'scale3d(1, 1, 1)',
+        'scale3d(1.4, .55, 1)',
+        'scale3d(.75, 1.25, 1)',
+        'scale3d(1.25, .85, 1)',
+        'scale3d(.9, 1.05, 1)',
+        'scale3d(1, 1, 1)',
       ],
-    })
-    setIsPlaying(true)
-  }
+    });
+    setIsPlaying(true);
+  };
 
   return (
     <motion.span
-      className='hover:text-[#08fdd8]'
+      className="hover:text-[#08fdd8]"
       animate={controls}
       onMouseOver={() => {
-        if (!isPlaying) rubberBand()
+        if (!isPlaying) rubberBand();
       }}
       onAnimationComplete={() => setIsPlaying(false)}
     >
       {children}
     </motion.span>
-  )
-}
+  );
+};
 
-export default TextSpan
+export default TextSpan;
