6bf9c801f197b71f2cf1b0acf3a579cbbe04030a
Gregory <gregory_berezin@outlook.com>
2023-05-20 11:21:30 +0300

    new component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import HeroLink from './UI/HeroLink';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="mt-5 flex justify-between items-center px-9 py-4 bg-[#d3f8cd] shadow-[#4c85e6]">
+      <p className="text-xl text-[#001b5e]">&copy; BG-coding 2023</p>
+      <HeroLink />
+    </footer>
+  );
+};
+
+export default Footer;
