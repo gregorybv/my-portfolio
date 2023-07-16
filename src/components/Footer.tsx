e426bc81a2a0b0353fa1e47672b4155830156649
Gregory <gregory_berezin@outlook.com>
2023-07-16 16:32:11 +0300

    new name my project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import HeroLink from './UI/HeroLink';
 const Footer: React.FC = () => {
   return (
     <footer className="mt-5 flex justify-between items-center px-9 py-4 bg-[#d3f8cd] shadow-[#4c85e6]">
-      <p className="text-xl text-[#001b5e]">&copy; G&Start 2023</p>
+      <p className="text-xl text-[#001b5e]">&copy; G&StArt 2023</p>
       <HeroLink />
     </footer>
   );
